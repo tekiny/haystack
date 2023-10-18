237f05d9e071c2b896a5e06b68b2d9ace1650757
Vladimir Blagojevic <dovlex@gmail.com>
2023-10-18 13:24:35 +0200

    Adapt ChatMessage to function calling

--- haystack/preview/dataclasses/chat_message.py
+++ haystack/preview/dataclasses/chat_message.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
-from typing import Literal
+from dataclasses import dataclass, field
+from typing import Literal, Dict, Any, Optional
 
 
 @dataclass
@@ -11,22 +11,24 @@ class ChatMessage:
 
     content: str
     role: str
+    name: Optional[str]
+    metadata: Optional[Dict[str, Any]] = field(default_factory=dict, hash=False)
 
     def is_from(self, role: Literal["assistant", "user", "system", "function"]) -> bool:
         return self.role == role
 
     @classmethod
     def from_assistant(cls, content: str) -> "ChatMessage":
-        return cls(content, ChatMessage.ASSISTANT)
+        return cls(content, ChatMessage.ASSISTANT, None)
 
     @classmethod
     def from_user(cls, content: str) -> "ChatMessage":
-        return cls(content, ChatMessage.USER)
+        return cls(content, ChatMessage.USER, None)
 
     @classmethod
     def from_system(cls, content: str) -> "ChatMessage":
-        return cls(content, ChatMessage.SYSTEM)
+        return cls(content, ChatMessage.SYSTEM, None)
 
     @classmethod
-    def from_function(cls, content: str) -> "ChatMessage":
-        return cls(content, ChatMessage.FUNCTION)
+    def from_function(cls, content: str, name: str) -> "ChatMessage":
+        return cls(content, ChatMessage.FUNCTION, name)
