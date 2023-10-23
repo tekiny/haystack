755fca2c045d973cbc1fe6573e3d9e8f4fe9babe
Vladimir Blagojevic <dovlex@gmail.com>
2023-10-23 16:31:00 +0200

    Update GPTGenerator to use ChatMessage, update tests

--- haystack/preview/components/builders/prompt_builder.py
+++ haystack/preview/components/builders/prompt_builder.py
@@ -1,9 +1,10 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional, List
 
 from jinja2 import Template, meta
 
 from haystack.preview import component
 from haystack.preview import default_to_dict
+from haystack.preview.dataclasses.chat_message import ChatMessage, ChatRole
 
 
 @component
@@ -20,22 +21,44 @@ class PromptBuilder:
     ```
     """
 
-    def __init__(self, template: str):
+    def __init__(self, template: Optional[str] = None, template_variables: Optional[List[str]] = None):
         """
-        Initialize the component with a template string.
+        Initialize the component with either a template string or template variables.
+        If template is given PromptBuilder will parse the template string and use the template variables
+        as input types. Conversely, if template_variables are given, PromptBuilder will directly use
+        them as input types.
 
-        :param template: Jinja2 template string, e.g. "Summarize this document: {documents}\nSummary:"
-        :type template: str
+        If neither template nor template_variables are provided, an error will be raised.
+
+        :param template: Template string to be rendered.
+        :param template_variables: List of template variables to be used as input types.
         """
-        self._template_string = template
-        self.template = Template(template)
-        ast = self.template.environment.parse(template)
-        template_variables = meta.find_undeclared_variables(ast)
-        component.set_input_types(self, **{var: Any for var in template_variables})
+        if template_variables:
+            dynamic_input_types = {var: Any for var in template_variables}
+        else:
+            if not template:
+                raise ValueError("Either template or template_variables must be provided.")
+            self._template_string = template
+            self.template = Template(template)
+            ast = self.template.environment.parse(template)
+            template_variables = meta.find_undeclared_variables(ast)
+            dynamic_input_types = {var: Any for var in template_variables}
+
+        static_input_type = {"messages": Optional[List[ChatMessage]]}
+        component.set_input_types(self, **static_input_type, **dynamic_input_types)
 
     def to_dict(self) -> Dict[str, Any]:
+        ## TODO properly serialize PromptBuilder
         return default_to_dict(self, template=self._template_string)
 
     @component.output_types(prompt=str)
-    def run(self, **kwargs):
-        return {"prompt": self.template.render(kwargs)}
+    def run(self, messages: Optional[List[ChatMessage]] = None, **kwargs):
+        if messages:
+            # apply the template to the last user message only
+            last_message: ChatMessage = messages[-1]
+            if last_message.is_from(ChatRole.USER):
+                template = Template(last_message.content)
+                messages[-1] = ChatMessage.from_user(template.render(kwargs))
+            return {"prompt": messages}
+        else:
+            return {"prompt": self.template.render(kwargs)}
